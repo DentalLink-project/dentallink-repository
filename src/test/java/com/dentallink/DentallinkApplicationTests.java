7b5f3b6bade81f58bae966a8c4d42a0ac650ffff
moominIsCute <1happymoomin1@gmail.com>
2025-10-15 00:46:59 +0900

    first commit

---
+++ src/test/java/com/dentallink/DentallinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentallink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentallinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
