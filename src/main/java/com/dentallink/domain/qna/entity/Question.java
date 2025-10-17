ebf9253cf8d71ec6fb2cd53da89fc339c8ca0fdc
ehy0419 <2hy0419@naver.com>
2025-10-17 13:56:13 +0900

    [Fix]-Qustion,Answer : 소프트 딜리트 날짜 삭제

--- src/main/java/com/dentallink/domain/qna/entity/Question.java
+++ src/main/java/com/dentallink/domain/qna/entity/Question.java
@@ -6,7 +6,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -31,9 +30,6 @@ public class Question extends BaseEntity {
     @Column(length = 255, nullable = false, columnDefinition = "TEXT")
     private String content;
 
-    @Column(name = "deleted_at")
-    private LocalDateTime deletedAt;
-
     // 질문 삭제 시 해당 질문에 달린 답변들도 함께 삭제
     @OneToMany(mappedBy = "question", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     private List<Answer> answerList = List.of();
@@ -54,15 +50,11 @@ public class Question extends BaseEntity {
         this.content = content;
     }
 
-    // 질문 소프트 삭제 메서드
+    // 질문 삭제 메서드 (soft delete)
     public void deleteQuestion() {
-        this.delete(); // BaseEntity의 soft_delete 메서드 호출
-        this.deletedAt = LocalDateTime.now();
-        // 필요 시 답변도 삭제 처리
+        this.delete();
         if (answerList != null) {
-            for (Answer answer : answerList) {
-                answer.delete(); // Answer 엔티티의 soft_delete 메서드 호출
-            }
+            answerList.forEach(Answer::deleteAnswer);
         }
     }
 }
