acc343dfd8f4afa0d39d019740d65834e44ac3e6
ehy0419 <2hy0419@naver.com>
2025-10-17 20:55:27 +0900

    [Fix]-Qustion,Answer : @Valid 어노테이션 추가

--- src/main/java/com/dentallink/domain/qna/dto/request/QuestionRequestDto.java
+++ src/main/java/com/dentallink/domain/qna/dto/request/QuestionRequestDto.java
@@ -1,9 +1,16 @@
 package com.dentallink.domain.qna.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class QuestionRequestDto {
 
     // 질문 등록 요청 DTO question create request dto
-    public record QuestionCreateRequest(Long hospitalId, String title, String content) {}
+    public record QuestionCreateRequest(
+            @NotNull Long hospitalId,
+            @NotBlank String title,
+            @NotBlank String content
+    ) {}
 
     // 질문 수정 요청 DTO question update request dto
     public record QuestionUpdateRequest(String title, String content) {}
