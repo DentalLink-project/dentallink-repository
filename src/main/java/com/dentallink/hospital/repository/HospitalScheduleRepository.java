aef9e57be0a609f76bc8b62cefaa4ea3ba0583ff
moominIsCute <1happymoomin1@gmail.com>
2025-10-18 00:54:53 +0900

    feat: HospitalScheduleRepository 병원 스케쥴 조회

--- src/main/java/com/dentallink/hospital/repository/HospitalScheduleRepository.java
+++ src/main/java/com/dentallink/hospital/repository/HospitalScheduleRepository.java
@@ -2,6 +2,15 @@ package com.dentallink.hospital.repository;
 
 import com.dentallink.hospital.entity.HospitalSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface HospitalScheduleRepository extends JpaRepository<HospitalSchedule, Integer> {
+
+    //병원 ID로 스케쥴 조회 석호
+    @Query("SELECT hs FROM HospitalSchedule hs " +
+            "WHERE hs.hospital.id = :hospitalId")
+    Optional<HospitalSchedule> findByHospitalId(@Param("hospitalId") Long hospitalId);
 }
