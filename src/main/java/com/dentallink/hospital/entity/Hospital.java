f60d63e9b13643889a4cf67268645a65c57a5927
t2023-m0121 <hardnokk@gmail.com>
2025-10-17 16:09:02 +0900

    feat: 병원 등록 기능 구현, 병원/병원 일정 엔티티 수정

--- src/main/java/com/dentallink/hospital/entity/Hospital.java
+++ src/main/java/com/dentallink/hospital/entity/Hospital.java
@@ -1,16 +1,20 @@
 package com.dentallink.hospital.entity;
 
+import com.dentallink.common.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Auditable;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class Hospital {
+public class Hospital extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
