1e84ad877ef9413afe891f57c60076f2329a5468
ehy0419 <2hy0419@naver.com>
2025-10-17 09:18:26 +0900

    [Feat]-Q&A:DTO 생성

---
+++ src/main/java/com/dentallink/qna/dto/response/AnswerResponseDto.java
@@ -0,0 +1,28 @@
+package com.dentallink.qna.dto.response;
+
+import com.dentallink.qna.entity.Answer;
+
+import java.time.LocalDateTime;
+
+public class AnswerResponseDto {
+
+    public record AnswerResponse(
+            Long Id,
+            Long questionId,
+            Long responderId,
+            String content,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt
+    ) {
+        public static AnswerResponse from(Answer answer) {
+            return new AnswerResponse(
+                    answer.getId(),
+                    answer.getQuestion().getId(),
+                    answer.getResponderId(),
+                    answer.getContent(),
+                    answer.getCreatedAt(),
+                    answer.getUpdatedAt()
+            );
+        }
+    }
+}
