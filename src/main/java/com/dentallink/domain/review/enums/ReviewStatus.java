f2e0378959dd8051f19bec59b2b2f350a3571d29
t2023-m0121 <hardnokk@gmail.com>
2025-10-20 20:39:25 +0900

    feat: entity, repository 작성

---
+++ src/main/java/com/dentallink/domain/review/enums/ReviewStatus.java
@@ -0,0 +1,12 @@
+package com.dentallink.domain.review.enums;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public enum ReviewStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
--- src/main/java/com/dentallink/domain/review/service/ReviewService.java
+++ src/main/java/com/dentallink/domain/review/service/ReviewService.java
@@ -1,4 +1,12 @@
 package com.dentallink.domain.review.service;
 
+import com.dentallink.domain.review.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
 }
