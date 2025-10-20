db91f7b350e38368b08b2a8ba0bfcd4fdd6bf18d
t2023-m0121 <hardnokk@gmail.com>
2025-10-20 14:48:53 +0900

    feat: HospitalListResponse에 final, (access = AccessLevel.PRIVATE) 추가

--- src/main/java/com/dentallink/domain/hospital/dto/response/HospitalListResponse.java
+++ src/main/java/com/dentallink/domain/hospital/dto/response/HospitalListResponse.java
@@ -1,16 +1,17 @@
 package com.dentallink.domain.hospital.dto.response;
 
 import com.dentallink.domain.hospital.entity.Hospital;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class HospitalListResponse {
-    private Long id;
-    private String hospitalName;
-    private String doctorName;
-    private Boolean isOpen;
+    private final Long id;
+    private final String hospitalName;
+    private final String doctorName;
+    private final Boolean isOpen;
 
     public static HospitalListResponse from(Hospital hospital) {
         return new HospitalListResponse(
