cfaa271d53d677b9ccf2b75dd69bc53b2ac7fa14
t2023-m0121 <hardnokk@gmail.com>
2025-10-20 20:39:25 +0900

    feat: entity, repository 작성

--- src/main/java/com/dentallink/domain/review/controller/ReviewController.java
+++ src/main/java/com/dentallink/domain/review/controller/ReviewController.java
@@ -1,4 +1,12 @@
 package com.dentallink.domain.review.controller;
 
+import com.dentallink.domain.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class ReviewController {
+    private final ReviewService reviewService;
+
 }
--- src/main/java/com/dentallink/domain/review/entity/Review.java
+++ src/main/java/com/dentallink/domain/review/entity/Review.java
@@ -1,4 +1,27 @@
 package com.dentallink.domain.review.entity;
 
-public class Review {
+import com.dentallink.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "reservation_id", nullable = false)
+    private Long reservationId;
+
+    @Column(name = "hospital_id", nullable = false)
+    private Long hospitalId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    private Integer point;
+    private String content;
 }
