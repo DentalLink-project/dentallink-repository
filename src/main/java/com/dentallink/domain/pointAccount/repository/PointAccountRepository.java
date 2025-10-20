b7ffc23d3c1d44b5cce84cc096f6c0813eb264dc
ksg <kasaga234@gmail.com>
2025-10-20 23:56:55 +0900

    fix-pointAccount controller user 연결

--- src/main/java/com/dentallink/domain/pointAccount/repository/PointAccountRepository.java
+++ src/main/java/com/dentallink/domain/pointAccount/repository/PointAccountRepository.java
@@ -1,7 +1,11 @@
 package com.dentallink.domain.pointAccount.repository;
 
 import com.dentallink.domain.pointAccount.entity.PointAccount;
+import com.dentallink.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PointAccountRepository extends JpaRepository<PointAccount,Long> {
+    Optional<PointAccount> findByUser(User user);
 }
