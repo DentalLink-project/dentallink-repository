33fc5b83252724c325d71ecbd121c36baa982a89
ksg <kasaga234@gmail.com>
2025-10-20 23:56:17 +0900

    fix-payment payment로 구매 시 pointaccount의 balance 변경

--- src/main/java/com/dentallink/domain/payment/controller/PaymentController.java
+++ src/main/java/com/dentallink/domain/payment/controller/PaymentController.java
@@ -4,13 +4,12 @@ import com.dentallink.common.response.ApiResponse;
 import com.dentallink.domain.payment.dto.request.PaymentRequest;
 import com.dentallink.domain.payment.dto.response.PaymentResponse;
 import com.dentallink.domain.payment.service.PaymentInternalService;
+import com.dentallink.domain.user.dto.security.AuthUser;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,11 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
     private final PaymentInternalService paymentInternalService;
 
-    /*@PostMapping("/deposit")
+    @PostMapping("/deposit")
     public ResponseEntity<ApiResponse<PaymentResponse>> depositPoint(
-            // 유저 아이디로 받은 account를 넘긴다.ㅇㄴㄹ.ㅜㅏㄴㅇ륳
-            @RequestBody PaymentRequest request) {
-        PaymentResponse response = paymentInternalService.depositPoint(request.accountId(), request.amount());
-        return ApiResponse.success(response, "포인트 충전에 성공했습니다.");
-    }*/
+            @AuthenticationPrincipal AuthUser authUser,
+            @RequestParam Long amount
+    ) {
+        PaymentResponse response = paymentInternalService.depositPoint(authUser.getUserId(), amount);
+        return ApiResponse.created(response, "포인트 충전(결제) 성공");
+    }
 }
