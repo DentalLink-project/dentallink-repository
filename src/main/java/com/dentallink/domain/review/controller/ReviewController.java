07aaf74c70ec291b434d687c3a5244c7849b98ee
t2023-m0121 <hardnokk@gmail.com>
2025-10-21 01:18:36 +0900

    feat: 병원 별 리뷰 목록 조회 기능 구현

--- src/main/java/com/dentallink/domain/review/controller/ReviewController.java
+++ src/main/java/com/dentallink/domain/review/controller/ReviewController.java
@@ -1,12 +1,30 @@
 package com.dentallink.domain.review.controller;
 
+import com.dentallink.domain.review.dto.response.ReviewListResponse;
 import com.dentallink.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/review")
 public class ReviewController {
     private final ReviewService reviewService;
 
+    // 병원 별 리뷰 목록 조회
+    @GetMapping("/{hospitalId}")
+    public ResponseEntity<Page<ReviewListResponse>> getAllReviews(
+            @PathVariable Long hospitalId,
+            @PageableDefault Pageable pageable
+    ) {
+        Page<ReviewListResponse> reviews = reviewService.findAllReviews(hospitalId, pageable);
+        return ResponseEntity.ok(reviews);
+    }
 }
--- src/main/java/com/dentallink/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/dentallink/domain/review/repository/ReviewRepository.java
@@ -1,9 +1,15 @@
 package com.dentallink.domain.review.repository;
 
 import com.dentallink.domain.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review,Long> {
+    @Query("SELECT r FROM Review r WHERE r.hospitalId = :hospitalId ORDER BY r.createdAt")
+    Page<Review> findByHospitalId(@Param("hospitalId") Long longId, Pageable pageable);
 }
