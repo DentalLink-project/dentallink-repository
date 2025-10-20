8a2e578cf6e5938955b374193bb211fcb695a43d
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-10-20 17:09:24 +0900

    feat - user: Controller, Entity 생성

---
+++ src/main/java/com/dentallink/domain/user/entity/User.java
@@ -0,0 +1,53 @@
+package com.dentallink.domain.user.entity;
+
+
+import com.dentallink.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import com.dentallink.domain.user.enums.UserRole;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String email;
+    private String password;
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    private User (String email, String password, String username, UserRole userRole) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.userRole = userRole;
+    }
+
+    public static User of(String email, String password, String username, UserRole userRole) {
+        return new User(email, password, username, userRole);
+    }
+
+    public void update(String username, String email) {
+        if(username != null) {
+            this.username = username;
+        }
+        if(email != null) {
+            this.email = email;
+        }
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
+}
