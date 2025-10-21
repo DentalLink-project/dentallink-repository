796a2f25d24096ab8fc339e5f44ce777fd70b0bd
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-10-21 14:52:09 +0900

    feat - favorites: 도메인명 수정 및 entity 추가

---
+++ src/main/java/com/dentallink/domain/bookmark/controller/FavoriteController.java
@@ -0,0 +1,14 @@
+package com.dentallink.domain.bookmark.controller;
+
+import com.dentallink.domain.bookmark.service.FavoriteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/hospitals/{hospitalId}/favorites")
+public class FavoriteController {
+
+    private final FavoriteService favoriteService;
+}
---
+++ src/main/java/com/dentallink/domain/bookmark/entity/Favorite.java
@@ -0,0 +1,46 @@
+package com.dentallink.domain.bookmark.entity;
+
+import com.dentallink.domain.hospital.entity.Hospital;
+import com.dentallink.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "favorites")
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospital;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    private Favorite (User user, Hospital hospital) {
+        this.user = user;
+        this.hospital = hospital;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public static Favorite of(User user, Hospital hospital) {
+        return new Favorite(user, hospital);
+    }
+}
