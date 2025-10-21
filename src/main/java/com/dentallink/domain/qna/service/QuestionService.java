d34e52ffe8f4fd6972e628ac01f196353bf01099
ehy0419 <2hy0419@naver.com>
2025-10-21 10:48:24 +0900

    [Docs]-Question, Answer : 질문 -> 문의 표현 수정

--- src/main/java/com/dentallink/domain/qna/service/QuestionService.java
+++ src/main/java/com/dentallink/domain/qna/service/QuestionService.java
@@ -39,7 +39,7 @@ public class QuestionService {
     // 비즈니스 로직 작성 update
     public void update(Long questionId, Long userId, String title, String content) {
         Question question = get(questionId);
-        // 본인 질문만 수정 가능
+        // 본인 문의만 수정 가능
         question.validateOwner(userId);
         question.updateQuestion(title, content);
     }
