4e01d8b5120bf0e786e941309b7ec2afbae0a180
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-10-21 14:52:09 +0900

    feat - favorites: 도메인명 수정 및 entity 추가

--- src/main/java/com/dentallink/domain/bookmark/repository/BookmarkRepository.java
+++
@@ -1,4 +0,0 @@
-package com.dentallink.domain.bookmark.repository;
-
-public interface BookmarkRepository {
-}
---
+++ src/main/java/com/dentallink/domain/bookmark/service/FavoriteService.java
@@ -0,0 +1,9 @@
+package com.dentallink.domain.bookmark.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FavoriteService {
+}
