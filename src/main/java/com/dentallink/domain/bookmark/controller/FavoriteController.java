ef1c1e92a6eb78d18619026ef36f31500c7bc2d3
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-10-21 17:39:23 +0900

    feat - favorites: 토글 기능 구현

--- src/main/java/com/dentallink/domain/bookmark/controller/FavoriteController.java
+++ src/main/java/com/dentallink/domain/bookmark/controller/FavoriteController.java
@@ -1,9 +1,17 @@
 package com.dentallink.domain.bookmark.controller;
 
+import com.dentallink.common.response.ApiResponse;
 import com.dentallink.domain.bookmark.service.FavoriteService;
+import com.dentallink.domain.user.dto.security.AuthUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.dentallink.common.response.ApiResponse.success;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class FavoriteController {
 
     private final FavoriteService favoriteService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> toggleFavorite(
+            @PathVariable Long hospitalId,
+            @AuthenticationPrincipal AuthUser authUser
+    ) {
+        return success(
+                null,
+                "즐겨찾기 "+favoriteService.toggleFavorite(hospitalId, authUser)+"되었습니다."
+        );
+    }
 }
