2378139c9f16c1e598bd2bd5fb086f21b3837ac7
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-10-22 20:32:11 +0900

    refactor - user: Service 분류 기준 변경

--- src/main/java/com/dentallink/domain/user/controller/UserAdminController.java
+++ src/main/java/com/dentallink/domain/user/controller/UserAdminController.java
@@ -3,8 +3,7 @@ package com.dentallink.domain.user.controller;
 import com.dentallink.common.response.PageResponse;
 import com.dentallink.common.response.ApiResponse;
 import com.dentallink.domain.user.dto.response.UserResponse;
-import com.dentallink.domain.user.service.command.UserCommandService;
-import com.dentallink.domain.user.service.query.UserQueryService;
+import com.dentallink.domain.user.service.UserInternalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,10 +14,9 @@ import static com.dentallink.common.response.ApiResponse.success;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
-public class UserInternalController {
+public class UserAdminController {
 
-    private final UserCommandService userCommandService;
-    private final UserQueryService userQueryService;
+    private final UserInternalService userInternalService;
 
     // 내부 사용자 전용(role 구분, admin만 가능한 내용)
 
@@ -30,19 +28,20 @@ public class UserInternalController {
             @PathVariable("userId") Long userId
     ) {
         return success(
-                userQueryService.getOneUser(userId),
+                userInternalService.getOneUser(userId),
                 "회원이 조회되었습니다."
                 );
     }
 
     // 모든 유저 열람
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<ApiResponse<PageResponse<UserResponse>>> getUsers(
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
         return  success(
-                userQueryService.getUsers(page, size),
+                userInternalService.getUsers(page, size),
                 "회원 목록이 조회되었습니다."
         );
     }
