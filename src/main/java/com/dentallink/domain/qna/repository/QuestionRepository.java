59f9906943e204b02f5b4ab185f97c523834f720
ehy0419 <2hy0419@naver.com>
2025-10-23 11:31:45 +0900

    [Feat]-Q&A: 문의 레포지토리 조회(정렬) 추가

--- src/main/java/com/dentallink/domain/qna/repository/QuestionRepository.java
+++ src/main/java/com/dentallink/domain/qna/repository/QuestionRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
@@ -58,4 +59,8 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
             @Param("keyword") String keyword,
             Pageable pageable
     );
+
+    List<Question> findAllByDeletedFalseOrderByCreatedAtDesc();
+
+    boolean existsByTitleAndDeletedFalse(String title);
 }
