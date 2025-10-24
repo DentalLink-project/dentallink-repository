f06e17612a396c9f6a7ba48233070c594924dc4f
t2023-m0121 <hardnokk@gmail.com>
2025-10-24 14:14:21 +0900

    feat: 사용하지 않는 import문 삭제

--- src/main/java/com/dentallink/domain/review/service/ReviewExternalService.java
+++ src/main/java/com/dentallink/domain/review/service/ReviewExternalService.java
@@ -8,7 +8,6 @@ import com.dentallink.domain.review.entity.Review;
 import com.dentallink.domain.review.exception.ReviewErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
