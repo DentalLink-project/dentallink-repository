7350d49584ea8501b25d0da99d57e6d1490149f0
t2023-m0121 <hardnokk@gmail.com>
2025-10-24 14:13:54 +0900

    feat: PageResponse 추가

--- src/main/java/com/dentallink/domain/review/controller/ReviewController.java
+++ src/main/java/com/dentallink/domain/review/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.dentallink.domain.review.controller;
 
 import com.dentallink.common.response.ApiResponse;
+import com.dentallink.common.response.PageResponse;
 import com.dentallink.domain.review.dto.request.ReviewCreateRequest;
 import com.dentallink.domain.review.dto.request.ReviewUpdateRequest;
 import com.dentallink.domain.review.dto.request.ReviewUpdateStatusRequest;
@@ -11,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -43,13 +43,14 @@ public class ReviewController {
 
     // 병원 별 리뷰 목록 조회
     @GetMapping("/hospitals/{hospitalId}/reviews")
-    public ResponseEntity<ApiResponse<Page<ReviewListResponse>>> getAllReviews(
+    public ResponseEntity<ApiResponse<PageResponse<ReviewListResponse>>> getAllReviews(
             @PathVariable Long hospitalId,
-            @PageableDefault Pageable pageable
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size
     ) {
-        Page<ReviewListResponse> reviews = reviewExternalService.findAllReviews(hospitalId, pageable);
         return success (
-                reviews, "리뷰 목록이 조회되었습니다."
+                reviewExternalService.findAllReviews(hospitalId, page, size),
+                "리뷰 목록이 조회되었습니다."
         );
     }
 
