59af48e718870376083550a4c2169a1b1d477b8c
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-10-27 20:00:16 +0900

    fix : 사용되지 않는 import문 삭제

--- src/main/java/com/dentallink/domain/auth/controller/AuthController.java
+++ src/main/java/com/dentallink/domain/auth/controller/AuthController.java
@@ -7,7 +7,6 @@ import com.dentallink.domain.auth.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
