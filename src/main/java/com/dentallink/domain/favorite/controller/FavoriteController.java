1ca43df326cae381dd5787b8f7054534d6c09d92
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-10-27 20:00:16 +0900

    fix : 사용되지 않는 import문 삭제

--- src/main/java/com/dentallink/domain/favorite/controller/FavoriteController.java
+++ src/main/java/com/dentallink/domain/favorite/controller/FavoriteController.java
@@ -7,7 +7,6 @@ import com.dentallink.domain.user.dto.security.AuthUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
