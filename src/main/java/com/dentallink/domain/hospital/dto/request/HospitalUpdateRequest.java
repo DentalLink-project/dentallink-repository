6504356e0beb9dc0d9aa6b2e3f22359ef43272d0
t2023-m0121 <hardnokk@gmail.com>
2025-10-29 16:16:24 +0900

    feat: reservationCost 추가

--- src/main/java/com/dentallink/domain/hospital/dto/request/HospitalUpdateRequest.java
+++ src/main/java/com/dentallink/domain/hospital/dto/request/HospitalUpdateRequest.java
@@ -8,6 +8,7 @@ public record HospitalUpdateRequest (
     String hospitalAddress,
     Boolean hospitalIsOpen,
     String doctorName,
+    Integer reservationCost,
     LocalTime openTime,
     LocalTime closeTime,
     LocalTime breakStart,
