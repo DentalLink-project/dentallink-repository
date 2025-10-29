065fd8de87c4e8f117a8daf8904ddf571e4c1f15
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-10-30 01:43:03 +0900

    Feature: Search Response/Service/Controller 생성

---
+++ src/main/java/com/dentallink/domain/search/controller/SearchController.java
@@ -0,0 +1,16 @@
+package com.dentallink.domain.search.controller;
+
+import com.dentallink.domain.search.service.SearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/search")
+public class SearchController {
+
+    private final SearchService searchService;
+
+
+}
---
+++ src/main/java/com/dentallink/domain/search/service/SearchServiceImpl.java
@@ -0,0 +1,7 @@
+package com.dentallink.domain.search.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchServiceImpl implements SearchService {
+}
