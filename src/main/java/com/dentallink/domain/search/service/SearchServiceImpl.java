f350333e222e1225de9bac25604233e3965a072f
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-10-31 15:46:20 +0900

    Feature: Search 기본 틀 작성

--- src/main/java/com/dentallink/domain/search/service/SearchServiceImpl.java
+++ src/main/java/com/dentallink/domain/search/service/SearchServiceImpl.java
@@ -1,7 +1,23 @@
 package com.dentallink.domain.search.service;
 
+import com.dentallink.common.response.PageResponse;
+import com.dentallink.domain.search.dto.response.SearchHospitalResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SearchServiceImpl implements SearchService {
+
+
+
+    @Override
+    public PageResponse<SearchHospitalResponse> getSearchHospital(int page, int size, String keyword) {
+        Pageable pageable = PageRequest.of(page > 0 ? page - 1 : 0, size);
+
+
+        return null;
+    }
 }
