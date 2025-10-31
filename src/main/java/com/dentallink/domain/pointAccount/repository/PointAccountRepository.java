e91a4bdd1701773c385b19628e0434acb0af3738
ksg <kasaga234@gmail.com>
2025-10-31 10:49:51 +0900

    feat/pointAccountLock

--- src/main/java/com/dentallink/domain/pointAccount/repository/PointAccountRepository.java
+++ src/main/java/com/dentallink/domain/pointAccount/repository/PointAccountRepository.java
@@ -12,12 +12,12 @@ public interface PointAccountRepository extends JpaRepository<PointAccount, Long
     // 기본 조회 (락 없음)
     Optional<PointAccount> findByUserId(Long userId);
 
-    // 🔒 비관적 락 (userId 기준)
+    // 비관적 락 (userId 기준)
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT p FROM PointAccount p WHERE p.user.id = :userId")
     Optional<PointAccount> findByUserIdWithLock(@Param("userId") Long userId);
 
-    // 🔒 비관적 락 (accountId 기준)
+    // 비관적 락 (accountId 기준)
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT p FROM PointAccount p WHERE p.id = :id")
     Optional<PointAccount> findByIdWithLock(@Param("id") Long id);
