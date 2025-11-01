15de330220e553147fd94ac671ad57ff7183e34c
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-11-02 08:55:26 +0900

    Feature: Auth - 토큰 재발급 및 여러 수정사항

--- src/main/java/com/dentallink/domain/auth/exception/AuthErrorCode.java
+++ src/main/java/com/dentallink/domain/auth/exception/AuthErrorCode.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum AuthErrorCode implements ErrorCode {
 
     NOT_AUTHENTICATED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효한 Refresh Token이 없습니다."),
     LOGIN_FAILED(HttpStatus.BAD_REQUEST, "로그인 정보가 일치하지 않습니다."),
     FORBIDDEN_USER(HttpStatus.FORBIDDEN, "권한이 없습니다.");
 
