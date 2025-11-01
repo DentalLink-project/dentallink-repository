4d56a7dbed5d5b6be8e9c3f9062f723ac10cf620
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-11-01 10:03:13 +0900

    Feature: Search N+1 무시하고 기능 작성

--- src/main/java/com/dentallink/domain/hospital/repository/HospitalRepository.java
+++ src/main/java/com/dentallink/domain/hospital/repository/HospitalRepository.java
@@ -1,9 +1,13 @@
 package com.dentallink.domain.hospital.repository;
 
 import com.dentallink.domain.hospital.entity.Hospital;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+
+    Page<Hospital> findAllByHospitalNameContainingAndDeletedIsFalse(Pageable pageable, String keyword);
 }
