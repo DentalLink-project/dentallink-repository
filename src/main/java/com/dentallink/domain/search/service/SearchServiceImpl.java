8b87a053dceb734aac829f9152a69ff4e543c7f3
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-11-01 10:03:13 +0900

    Feature: Search N+1 무시하고 기능 작성

--- src/main/java/com/dentallink/domain/search/service/SearchServiceImpl.java
+++ src/main/java/com/dentallink/domain/search/service/SearchServiceImpl.java
@@ -1,23 +1,40 @@
 package com.dentallink.domain.search.service;
 
 import com.dentallink.common.response.PageResponse;
+import com.dentallink.domain.hospital.entity.Hospital;
+import com.dentallink.domain.hospital.entity.HospitalSchedule;
+import com.dentallink.domain.hospital.repository.HospitalScheduleRepository;
+import com.dentallink.domain.hospital.service.HospitalExternalService;
 import com.dentallink.domain.search.dto.response.SearchHospitalResponse;
+import com.dentallink.domain.user.dto.response.UserResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.MergedAnnotations;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SearchServiceImpl implements SearchService {
 
-
+    private final HospitalExternalService hospitalExternalService;
+    private final HospitalScheduleRepository hospitalScheduleRepository;
 
     @Override
     public PageResponse<SearchHospitalResponse> getSearchHospital(int page, int size, String keyword) {
+
         Pageable pageable = PageRequest.of(page > 0 ? page - 1 : 0, size);
+        Page<Hospital> hospitals = hospitalExternalService.getHospitalsByKeyword(pageable, keyword);
 
+        Page<SearchHospitalResponse> response = hospitals.map(
+                hospital -> SearchHospitalResponse.from(hospital, hospital.getHospitalSchedule())
+        );
 
-        return null;
+        return PageResponse.fromPage(response);
     }
 }
