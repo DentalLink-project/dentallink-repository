9084f62ad1c41a796951cc79ec927f4db7c6f2fa
moominIsCute <1happymoomin1@gmail.com>
2025-11-13 20:59:14 +0900

    Switch to standard Redis cluster and remove SSL/Redisson config
    - Update Redis endpoint to new cluster (master.prod-redis-cluster)
    - Remove SSL/TLS configuration (standard cluster doesn't require it)
    - Disable Redisson auto-configuration to avoid connection issues
    - Keep RedisTemplate for basic Redis operations

--- src/main/java/com/dentallink/common/config/RedisConfig.java
+++ src/main/java/com/dentallink/common/config/RedisConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -15,42 +14,28 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
 
-    // yml 값을 주입합니다.
-    @Value("${spring.data.redis.host}") // 호스트
+    @Value("${spring.data.redis.host}")
     private String host;
-    @Value("${spring.data.redis.port}") // 포트
+    @Value("${spring.data.redis.port}")
     private int port;
 
-    // Redis 연결 팩토리 설정
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        // Redis 설정
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(host);
         redisStandaloneConfiguration.setPort(port);
 
-        // SSL/TLS 활성화 (ElastiCache Serverless 필수)
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .useSsl()
-                .build();
-
-        // Lettuce 선택했습니다. (비동기 처리) + SSL 설정 적용
-        return new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
-    // RedisTemplate 설정 (Set, Get, Delete ... etc)
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
-
-        // 통신 템플릿
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
 
-        // key-value 직렬화 방법 설정
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
 
-        // hash key-hash value 직렬화 방법 설정
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(new StringRedisSerializer());
 
