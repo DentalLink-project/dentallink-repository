251ae40fdfa84777e2251b612cc0b4c1e9e06bd3
moominIsCute <1happymoomin1@gmail.com>
2025-11-18 22:22:02 +0900

    도메인 연결시도 v1.0

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -4,7 +4,8 @@
 // --- [전역 설정] ---
 window.API_URL = window.location.hostname === 'localhost'
     ? 'http://localhost:8080'
-    : window.location.origin;
+    : 'https://www.dentallink.store
+
 // --- [토큰 관리] ---
 function getToken() {
     return localStorage.getItem('token');
